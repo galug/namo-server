2cc64f9e0e6f478281a525bbba686cf5fac7e7cb
김현재 <khjmr@naver.com>
2023-09-12 16:27:18 +0900

    fix(logoutreq): dto 생성자 버그  수정

--- src/main/java/com/example/namo2/auth/dto/LogoutReq.java
+++ src/main/java/com/example/namo2/auth/dto/LogoutReq.java
@@ -2,6 +2,7 @@ package com.example.namo2.auth.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
@@ -9,6 +10,7 @@ import javax.validation.constraints.NotBlank;
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LogoutReq {
     @NotBlank
     private String accessToken;
