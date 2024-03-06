e5341d6e6f75e606c5c50dbfb9f019e071e049b5
김현재 <khjmr@naver.com>
2024-03-06 19:43:36 +0900

    :sparkles: Feat: 약관 동의 API 추가
    <body>
    - 약관 동의 API 추가
    
    <footer>
    - 관련: #37

--- src/main/java/com/example/namo2/domain/user/application/impl/UserService.java
+++ src/main/java/com/example/namo2/domain/user/application/impl/UserService.java
@@ -4,7 +4,12 @@ import static com.example.namo2.global.common.response.BaseResponseStatus.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.TreeMap;
 
+import com.example.namo2.domain.user.dao.repository.TermRepository;
+import com.example.namo2.domain.user.domain.Term;
+import com.example.namo2.global.common.response.BaseResponseStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import com.example.namo2.domain.user.dao.repository.UserRepository;
@@ -16,34 +21,49 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final TermRepository termRepository;
 
-	public User createUser(User user) {
-		return userRepository.save(user);
-	}
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
 
-	public User getUser(Long userId) {
-		return userRepository.findById(userId)
-			.orElseThrow(() -> new BaseException(NOT_FOUND_USER_FAILURE));
-	}
+    public User getUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new BaseException(NOT_FOUND_USER_FAILURE));
+    }
 
-	public List<User> getUsers(List<Long> users) {
-		return userRepository.findUsersById(users);
-	}
+    public List<User> getUsers(List<Long> users) {
+        return userRepository.findUsersById(users);
+    }
 
-	public Optional<User> getUserByEmail(String email) {
-		return userRepository.findUserByEmail(email);
-	}
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findUserByEmail(email);
+    }
 
-	public User getUserByRefreshToken(String refreshToken) {
-		return userRepository.findUserByRefreshToken(refreshToken)
-			.orElseThrow(() -> new BaseException(NOT_FOUND_USER_FAILURE));
-	}
+    public User getUserByRefreshToken(String refreshToken) {
+        return userRepository.findUserByRefreshToken(refreshToken)
+                .orElseThrow(() -> new BaseException(NOT_FOUND_USER_FAILURE));
+    }
 
-	public void updateRefreshToken(Long userId, String refreshToken) {
-		User user = userRepository.findById(userId).orElseThrow(() -> new BaseException(NOT_FOUND_USER_FAILURE));
-		user.updateRefreshToken(refreshToken);
-	}
+    public void updateRefreshToken(Long userId, String refreshToken) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new BaseException(NOT_FOUND_USER_FAILURE));
+        user.updateRefreshToken(refreshToken);
+    }
+
+    public void createTerm(List<Term> terms) {
+        for (Term term : terms) {
+            if (!term.getIsCheck()) {
+                throw new BaseException(NOT_CHECK_TERM_ERROR);
+            }
+            termRepository.findTermByContentAndUser(term.getContent(), term.getUser())
+                    .ifPresentOrElse(
+                            savedTerm -> savedTerm.update(),
+                            () -> termRepository.save(term)
+                    );
+        }
+    }
 }
