905913f4ce8c3bb2ac225edc6c118b59a155213c
김현재 <khjmr@naver.com>
2024-03-07 20:27:57 +0900

    :art: Style: 코드 형식에 맞게 포맷팅
    <body>
    - 코드 스타일 포맷에 맞게 재정의
    
    <footer>
    - 관련: #37

--- src/main/java/com/example/namo2/domain/user/application/converter/TermConverter.java
+++ src/main/java/com/example/namo2/domain/user/application/converter/TermConverter.java
@@ -1,31 +1,29 @@
 package com.example.namo2.domain.user.application.converter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.namo2.domain.user.domain.Content;
 import com.example.namo2.domain.user.domain.Term;
 import com.example.namo2.domain.user.domain.User;
 import com.example.namo2.domain.user.ui.dto.UserRequest;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class TermConverter {
-    public static List<Term> toTerms(UserRequest.TermDto termDto, User user) {
-        List<Term> terms = new ArrayList<>();
-        System.out.println("termOfUse" + termDto.getIsCheckTermOfUse());
-        System.out.println("termOfUse" + termDto.getIsCheckPersonalInformationCollection());
-        Term termOfUse = toTerm(Content.TERMS_OF_USE, termDto.getIsCheckTermOfUse(), user);
-        Term termOfPersonal
-                = toTerm(Content.PERSONAL_INFORMATION_COLLECTION, termDto.getIsCheckPersonalInformationCollection(), user);
-        terms.add(termOfUse);
-        terms.add(termOfPersonal);
-        return terms;
-    }
+	public static List<Term> toTerms(UserRequest.TermDto termDto, User user) {
+		List<Term> terms = new ArrayList<>();
+		Term termOfUse = toTerm(Content.TERMS_OF_USE, termDto.getIsCheckTermOfUse(), user);
+		Term termOfPersonal
+			= toTerm(Content.PERSONAL_INFORMATION_COLLECTION, termDto.getIsCheckPersonalInformationCollection(), user);
+		terms.add(termOfUse);
+		terms.add(termOfPersonal);
+		return terms;
+	}
 
-    private static Term toTerm(Content content, Boolean isCheck, User user) {
-        return Term.builder()
-                .user(user)
-                .content(content)
-                .isCheck(isCheck)
-                .build();
-    }
+	private static Term toTerm(Content content, Boolean isCheck, User user) {
+		return Term.builder()
+			.user(user)
+			.content(content)
+			.isCheck(isCheck)
+			.build();
+	}
 }
