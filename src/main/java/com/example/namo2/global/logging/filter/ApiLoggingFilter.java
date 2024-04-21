732825d9c85066f3fdd8f33c82a8cba691ef2c1b
luke0408 <zjvl324@gmail.com>
2024-04-21 18:35:02 +0900

    :wrench: Chore: Swagger 패키지 스캔 설정 수정
    <footer>
    - 관련: #82

--- src/main/java/com/example/namo2/global/logging/filter/ApiLoggingFilter.java
+++ src/main/java/com/example/namo2/global/logging/filter/ApiLoggingFilter.java
@@ -73,7 +73,6 @@ public class ApiLoggingFilter extends OncePerRequestFilter {
 
 	private static void logPayload(String prefix, String contentType, byte[] rowData) throws IOException {
 		boolean visible = isVisible(MediaType.valueOf(contentType == null ? "application/json" : contentType));
-		log.info("{} Content-Type: {}", prefix, contentType);
 
 		if (visible) {
 			if (rowData.length > 0) {
