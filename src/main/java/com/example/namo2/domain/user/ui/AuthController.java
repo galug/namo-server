d2186bd26a7b6f9c5d409ecabfc90afa8bfb0211
luke0408 <zjvl324@gmail.com>
2024-04-26 21:07:14 +0900

    :sparkles: Feat: AuthController 기본 에러 관련 설명 추가
    <footer>
    - 관련: #90

--- src/main/java/com/example/namo2/domain/user/ui/AuthController.java
+++ src/main/java/com/example/namo2/domain/user/ui/AuthController.java
@@ -1,22 +1,21 @@
 package com.example.namo2.domain.user.ui;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
 import com.example.namo2.domain.user.application.UserFacade;
 import com.example.namo2.domain.user.ui.dto.UserRequest;
 import com.example.namo2.domain.user.ui.dto.UserResponse;
-
+import com.example.namo2.global.annotation.swagger.ApiErrorCodes;
 import com.example.namo2.global.common.response.BaseResponse;
+import com.example.namo2.global.common.response.BaseResponseStatus;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Tag(name = "Auth", description = "로그인, 회원가입 관련 API")
@@ -28,6 +27,19 @@ public class AuthController {
 
 	@Operation(summary = "kakao 회원가입", description = "kakao 소셜 로그인을 통한 회원가입")
 	@PostMapping(value = "/kakao/signup")
+	@ApiErrorCodes({
+			BaseResponseStatus.USER_POST_ERROR,
+			BaseResponseStatus.KAKAO_UNAUTHORIZED,
+			BaseResponseStatus.KAKAO_FORBIDDEN,
+			BaseResponseStatus.KAKAO_BAD_GATEWAY,
+			BaseResponseStatus.KAKAO_SERVICE_UNAVAILABLE,
+			BaseResponseStatus.KAKAO_INTERNAL_SERVER_ERROR,
+			BaseResponseStatus.SOCIAL_LOGIN_FAILURE,
+			BaseResponseStatus.EMPTY_ACCESS_KEY,
+			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+			BaseResponseStatus.INTERNET_SERVER_ERROR
+	})
 	public BaseResponse<UserResponse.SignUpDto> kakaoSignup(
 		@Valid @RequestBody UserRequest.SocialSignUpDto signUpDto
 	) {
@@ -37,6 +49,16 @@ public class AuthController {
 
 	@Operation(summary = "naver 회원가입", description = "naver 소셜 로그인을 통한 회원가입")
 	@PostMapping(value = "/naver/signup")
+	@ApiErrorCodes({
+			BaseResponseStatus.USER_POST_ERROR,
+			BaseResponseStatus.NAVER_UNAUTHORIZED,
+			BaseResponseStatus.NAVER_FORBIDDEN,
+			BaseResponseStatus.SOCIAL_LOGIN_FAILURE,
+			BaseResponseStatus.EMPTY_ACCESS_KEY,
+			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+			BaseResponseStatus.INTERNET_SERVER_ERROR
+	})
 	public BaseResponse<UserResponse.SignUpDto> naverSignup(
 		@Valid @RequestBody UserRequest.SocialSignUpDto signUpDto
 	) {
@@ -46,6 +68,14 @@ public class AuthController {
 
 	@Operation(summary = "apple 회원가입", description = "apple 소셜 로그인을 통한 회원가입.")
 	@PostMapping(value = "/apple/signup")
+	@ApiErrorCodes({
+			BaseResponseStatus.USER_POST_ERROR,
+			BaseResponseStatus.MAKE_PUBLIC_KEY_FAILURE,
+			BaseResponseStatus.APPLE_REQUEST_ERROR,
+			BaseResponseStatus.APPLE_UNAUTHORIZED,
+			BaseResponseStatus.SOCIAL_LOGIN_FAILURE,
+			BaseResponseStatus.FEIGN_SERVER_ERROR
+	})
 	public BaseResponse<UserResponse.SignUpDto> appleSignup(
 		@Valid @RequestBody UserRequest.AppleSignUpDto dto
 	) {
@@ -55,6 +85,14 @@ public class AuthController {
 
 	@Operation(summary = "토큰 재발급", description = "토큰 재발급")
 	@PostMapping(value = "/reissuance")
+	@ApiErrorCodes({
+			BaseResponseStatus.USER_POST_ERROR,
+			BaseResponseStatus.SOCIAL_LOGIN_FAILURE,
+			BaseResponseStatus.EMPTY_ACCESS_KEY,
+			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+			BaseResponseStatus.INTERNET_SERVER_ERROR
+	})
 	public BaseResponse<UserResponse.ReissueDto> reissueAccessToken(
 		@Valid @RequestBody UserRequest.SignUpDto signUpDto
 	) {
@@ -64,7 +102,15 @@ public class AuthController {
 
 	@Operation(summary = "로그아웃", description = "로그아웃")
 	@PostMapping(value = "/logout")
-	public BaseResponse logout(
+	@ApiErrorCodes({
+			BaseResponseStatus.USER_POST_ERROR,
+			BaseResponseStatus.SOCIAL_LOGIN_FAILURE,
+			BaseResponseStatus.EMPTY_ACCESS_KEY,
+			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+			BaseResponseStatus.INTERNET_SERVER_ERROR
+	})
+	public BaseResponse<Void> logout(
 		@Valid @RequestBody UserRequest.LogoutDto logoutDto
 	) {
 		userFacade.logout(logoutDto);
@@ -73,6 +119,12 @@ public class AuthController {
 
 	@Operation(summary = "kakao 회원 탈퇴", description = "kakao 회원 탈퇴")
 	@PostMapping("/kakao/delete")
+	@ApiErrorCodes({
+			BaseResponseStatus.EMPTY_ACCESS_KEY,
+			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+			BaseResponseStatus.INTERNET_SERVER_ERROR
+	})
 	public BaseResponse<?> removeKakaoUser(
 		HttpServletRequest request,
 		@Valid @RequestBody UserRequest.DeleteUserDto deleteUserDto
@@ -83,6 +135,12 @@ public class AuthController {
 
 	@Operation(summary = "네이버 회원 탈퇴", description = "네이버 회원 탈퇴")
 	@PostMapping("/naver/delete")
+	@ApiErrorCodes({
+			BaseResponseStatus.EMPTY_ACCESS_KEY,
+			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+			BaseResponseStatus.INTERNET_SERVER_ERROR
+	})
 	public BaseResponse<?> removeNaverUser(
 		HttpServletRequest request,
 		@Valid @RequestBody UserRequest.DeleteUserDto deleteUserDto
@@ -94,6 +152,12 @@ public class AuthController {
 	@SuppressWarnings({"checkstyle:WhitespaceAround", "checkstyle:RegexpMultiline"})
 	@Operation(summary = "애플 회원 탈퇴", description = "애플 회원 탈퇴")
 	@PostMapping("/apple/delete")
+	@ApiErrorCodes({
+			BaseResponseStatus.EMPTY_ACCESS_KEY,
+			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+			BaseResponseStatus.INTERNET_SERVER_ERROR
+	})
 	public BaseResponse<?> removeAppleUser(
 		HttpServletRequest request,
 		@Valid @RequestBody UserRequest.DeleteAppleUserDto deleteAppleUserDto
