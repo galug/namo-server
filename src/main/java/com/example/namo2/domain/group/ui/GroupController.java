582168f968da7c1929f0c614ebcb30631546657d
luke0408 <zjvl324@gmail.com>
2024-05-07 18:54:37 +0900

    :memo: Docs: Group 도메인 api 설명 추가
    <body>
    - Parameter에 대한 설명 추가
      - 파라미터 기준: pathVariable, requestPart
    
    <footer>
    - 관련: #105

--- src/main/java/com/example/namo2/domain/group/ui/GroupController.java
+++ src/main/java/com/example/namo2/domain/group/ui/GroupController.java
@@ -2,6 +2,9 @@ package com.example.namo2.domain.group.ui;
 
 import java.util.List;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,14 +20,15 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.namo2.domain.group.application.MoimFacade;
 import com.example.namo2.domain.group.ui.dto.GroupRequest;
 import com.example.namo2.domain.group.ui.dto.GroupResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import com.example.namo2.global.annotation.swagger.ApiErrorCodes;
 import com.example.namo2.global.common.response.BaseResponse;
 import com.example.namo2.global.common.response.BaseResponseStatus;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -37,82 +41,90 @@ public class GroupController {
 	private final MoimFacade moimFacade;
 
 	@Operation(summary = "그룹 생성", description = "그룹 생성 API")
-	@PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@PostMapping(value = "",
+		consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+		produces = MediaType.APPLICATION_JSON_VALUE
+	)
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<GroupResponse.GroupIdDto> createMoim(
-			@RequestPart(required = false) MultipartFile img,
-			@RequestPart(required = true) String groupName,
-			HttpServletRequest request
+		@Parameter(description = "그룹 프로필 img") @RequestPart(required = false) MultipartFile img,
+		@Parameter(description = "그룹명") @RequestPart(required = true) String groupName,
+		HttpServletRequest request
 	) {
-		GroupResponse.GroupIdDto groupIdDto = moimFacade.createMoim((Long)request.getAttribute("userId"), groupName, img);
-		return new BaseResponse(groupIdDto);
+		GroupResponse.GroupIdDto groupIdDto = moimFacade.createMoim(
+			(Long)request.getAttribute("userId"),
+			groupName,
+			img
+		);
+
+		return new BaseResponse<>(groupIdDto);
 	}
 
 	@Operation(summary = "그룹 조회", description = "유저가 참여중인 그룹 조회 API")
 	@GetMapping("")
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<List<GroupResponse.GroupDto>> findMoims(
-			HttpServletRequest request
+		HttpServletRequest request
 	) {
 		List<GroupResponse.GroupDto> moims = moimFacade.getMoims((Long)request.getAttribute("userId"));
-		return new BaseResponse(moims);
+		return new BaseResponse<>(moims);
 	}
 
 	@Operation(summary = "그룹 이름 수정", description = "그룹 이름 수정 API, 변경자의 입장에서만 수정되어 적용됨")
 	@PatchMapping("/name")
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<Long> modifyMoimName(
-			@Valid @RequestBody GroupRequest.PatchGroupNameDto patchGroupNameDto,
-			HttpServletRequest request
+		@Valid @RequestBody GroupRequest.PatchGroupNameDto patchGroupNameDto,
+		HttpServletRequest request
 	) {
 		Long moimId = moimFacade.modifyMoimName(patchGroupNameDto, (Long)request.getAttribute("userId"));
-		return new BaseResponse(moimId);
+		return new BaseResponse<>(moimId);
 	}
 
 	@Operation(summary = "그룹 참여", description = "그룹 참여 API")
 	@PatchMapping("/participate/{code}")
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<GroupResponse.GroupParticipantDto> createMoimAndUser(
-			@PathVariable("code") String code,
-			HttpServletRequest request
+		@Parameter(description = "그룹 참여용 코드") @PathVariable("code") String code,
+		HttpServletRequest request
 	) {
 		GroupResponse.GroupParticipantDto groupParticipantDto = moimFacade.createMoimAndUser(
-				(Long)request.getAttribute("userId"),
-				code);
-		return new BaseResponse(groupParticipantDto);
+			(Long)request.getAttribute("userId"),
+			code);
+		return new BaseResponse<>(groupParticipantDto);
 	}
 
 	@Operation(summary = "그룹 탈퇴", description = "그룹 탈퇴 API")
 	@DeleteMapping("/withdraw/{groupId}")
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse removeMoimAndUser(
-			@PathVariable("groupId") Long groupId,
-			HttpServletRequest request
+		@Parameter(description = "그룹 ID") @PathVariable("groupId") Long groupId,
+		HttpServletRequest request
 	) {
 		moimFacade.removeMoimAndUser((Long)request.getAttribute("userId"), groupId);
 		return BaseResponse.ok();
--- src/main/java/com/example/namo2/domain/group/ui/GroupDiaryController.java
+++ src/main/java/com/example/namo2/domain/group/ui/GroupDiaryController.java
@@ -24,6 +24,7 @@ import com.example.namo2.domain.group.ui.dto.GroupDiaryResponse;
 import com.example.namo2.domain.group.ui.dto.GroupScheduleRequest;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import com.example.namo2.global.annotation.swagger.ApiErrorCodes;
@@ -45,18 +46,18 @@ public class GroupDiaryController {
 
 	@Operation(summary = "모임 기록 생성", description = "모임 기록 생성 API")
 	@PostMapping(value = "/{moimScheduleId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<Void> createMoimMemo(
-			@PathVariable Long moimScheduleId,
-			@RequestPart(required = false) List<MultipartFile> imgs,
-			@RequestPart String name,
-			@RequestPart String money,
-			@RequestPart String participants
+		@Parameter(description = "모임 일정 ID") @PathVariable Long moimScheduleId,
+		@Parameter(description = "모임 기록용 이미지") @RequestPart(required = false) List<MultipartFile> imgs,
+		@Parameter(description = "모임 기록명") @RequestPart String name,
+		@Parameter(description = "모임 회비") @RequestPart String money,
+		@Parameter(description = "참여자", example = "멍청이, 똑똑이") @RequestPart String participants
 	) {
 		GroupDiaryRequest.LocationDto locationDto = new GroupDiaryRequest.LocationDto(name, money, participants);
 		moimMemoFacade.create(moimScheduleId, locationDto, imgs);
@@ -65,18 +66,18 @@ public class GroupDiaryController {
 
 	@Operation(summary = "모임 기록 장소 수정", description = "모임 기록 장소 수정 API")
 	@PatchMapping(value = "/{activityId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<Object> updateMoimMemo(
-			@PathVariable Long activityId,
-			@RequestPart(required = false) List<MultipartFile> imgs,
-			@RequestPart String name,
-			@RequestPart String money,
-			@RequestPart String participants
+		@Parameter(description = "수정하고자 하는 활동 ID") @PathVariable Long activityId,
+		@Parameter(description = "모임 기록용 이미지") @RequestPart(required = false) List<MultipartFile> imgs,
+		@Parameter(description = "모임 기록명") @RequestPart String name,
+		@Parameter(description = "모임 회비") @RequestPart String money,
+		@Parameter(description = "참여자", example = "멍청이, 똑똑이") @RequestPart String participants
 	) {
 		GroupDiaryRequest.LocationDto locationDto = new GroupDiaryRequest.LocationDto(name, money, participants);
 		moimMemoFacade.modifyMoimMemoLocation(activityId, locationDto, imgs);
@@ -85,14 +86,14 @@ public class GroupDiaryController {
 
 	@Operation(summary = "모임 기록 조회", description = "모임 기록 조회 API")
 	@GetMapping("/{moimScheduleId}")
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<Object> getMoimMemo(
-			@PathVariable("moimScheduleId") Long moimScheduleId
+		@Parameter(description = "모임 기록 ID") @PathVariable("moimScheduleId") Long moimScheduleId
 	) {
 		GroupDiaryResponse.MoimDiaryDto moimDiaryDto = moimMemoFacade.getMoimMemoWithLocations(moimScheduleId);
 		return new BaseResponse(moimDiaryDto);
@@ -100,34 +101,34 @@ public class GroupDiaryController {
 
 	@Operation(summary = "월간 모임 기록 조회", description = "월간 모임 기록 조회 API")
 	@GetMapping("/month/{month}")
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<GroupDiaryResponse.SliceDiaryDto<GroupDiaryResponse.DiaryDto>> findMonthMoimMemo(
-			@PathVariable("month") String month,
-			Pageable pageable,
-			HttpServletRequest request
+		@Parameter(description = "조회 일자", example = "{년},{월}") @PathVariable("month") String month,
+		Pageable pageable,
+		HttpServletRequest request
 	) {
 		List<LocalDateTime> localDateTimes = converter.convertLongToLocalDateTime(month);
 		GroupDiaryResponse.SliceDiaryDto<GroupDiaryResponse.DiaryDto> diaryDto = moimMemoFacade
-				.getMonthMonthMoimMemo((Long)request.getAttribute("userId"), localDateTimes, pageable);
+			.getMonthMonthMoimMemo((Long)request.getAttribute("userId"), localDateTimes, pageable);
 		return new BaseResponse(diaryDto);
 	}
 
-	@Operation(summary = "개인 페이지 모임 기록 삭제", description = "모임 기록 장소 삭제 API")
+	@Operation(summary = "개인 페이지 모임 기록 삭제", description = "일정에 대한 모임 활동 기록 삭제 API")
 	@DeleteMapping("/person/{scheduleId}")
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<Object> removePersonMoimMemo(
-			@PathVariable Long scheduleId,
-			HttpServletRequest request
+		@Parameter(description = "일정 ID") @PathVariable Long scheduleId,
+		HttpServletRequest request
 	) {
 		Long userId = (Long)request.getAttribute("userId");
 		moimMemoFacade.removePersonMoimMemo(scheduleId, userId);
@@ -136,50 +137,50 @@ public class GroupDiaryController {
 
 	@Operation(summary = "모임 기록 전체 삭제", description = "모임 기록 전체 삭제 API")
 	@DeleteMapping("/all/{moimDiaryId}")
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<Object> removeMoimMemo(
-			@PathVariable Long moimDiaryId
+		@Parameter(description = "모임 기록 ID") @PathVariable Long moimDiaryId
 	) {
 		moimMemoFacade.removeMoimMemo(moimDiaryId);
 		return BaseResponse.ok();
 	}
 
-	@Operation(summary = "모임 기록 장소 삭제", description = "모임 기록 장소 삭제 API")
+	@Operation(summary = "모임 활동 삭제", description = "모임 활동 삭제 API")
 	@DeleteMapping("/{activityId}")
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<Object> removeMoimMemoLocation(
-			@PathVariable Long activityId
+		@Parameter(description = "모임 활동 ID") @PathVariable Long activityId
 	) {
 		moimMemoFacade.removeMoimMemoLocation(activityId);
 		return BaseResponse.ok();
 	}
 
-	@Operation(summary = "모임 기록 텍스트 추가", description = "모임 기록 텍스트 추가 API")
+	@Operation(summary = "모임 기록 텍스트 추가", description = "모임 기록 추가 API")
 	@PatchMapping("/text/{moimScheduleId}")
-	@ApiErrorCodes({
-			BaseResponseStatus.EMPTY_ACCESS_KEY,
-			BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
-			BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
-			BaseResponseStatus.INTERNET_SERVER_ERROR
+	@ApiErrorCodes(value = {
+		BaseResponseStatus.EMPTY_ACCESS_KEY,
+		BaseResponseStatus.EXPIRATION_ACCESS_TOKEN,
+		BaseResponseStatus.EXPIRATION_REFRESH_TOKEN,
+		BaseResponseStatus.INTERNET_SERVER_ERROR
 	})
 	public BaseResponse<Object> createMoimScheduleText(
-			@PathVariable Long moimScheduleId,
-			@RequestBody GroupScheduleRequest.PostGroupScheduleTextDto moimScheduleText,
-			HttpServletRequest request
+		@Parameter(description = "모임 일정 ID") @PathVariable Long moimScheduleId,
+		@RequestBody GroupScheduleRequest.PostGroupScheduleTextDto moimScheduleText,
+		HttpServletRequest request
 	) {
 		moimMemoFacade.createMoimScheduleText(moimScheduleId,
-				(Long)request.getAttribute("userId"),
-				moimScheduleText);
+			(Long)request.getAttribute("userId"),
+			moimScheduleText);
 		return BaseResponse.ok();
 	}
 }
