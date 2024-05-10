29199bb9b3b87cda1bb286f98f3fdadb6b3da7fc
luke0408 <zjvl324@gmail.com>
2024-05-11 00:36:13 +0900

    :sparkles: Feat: Multipart 요청에 대한 request 값 로깅 기능 추가
    <footer>
    - 관련: #107

--- src/main/java/com/example/namo2/global/config/logging/ApiLoggingFilter.java
+++ src/main/java/com/example/namo2/global/config/logging/ApiLoggingFilter.java
@@ -3,20 +3,27 @@ package com.example.namo2.global.config.logging;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -25,25 +32,25 @@ public class ApiLoggingFilter extends OncePerRequestFilter {
 
 	@Override
 	protected void doFilterInternal(
-			HttpServletRequest request,
-			HttpServletResponse response,
-			FilterChain filterChain
+		HttpServletRequest request,
+		HttpServletResponse response,
+		FilterChain filterChain
 	) throws ServletException, IOException {
 		if (isAsyncDispatch(request)) {
 			filterChain.doFilter(request, response);
 		} else {
 			doFilterWrapped(
-					new ContentCachingRequestWrapper(request),
-					new ContentCachingResponseWrapper(response),
-					filterChain
+				new ContentCachingRequestWrapper(request),
+				new ContentCachingResponseWrapper(response),
+				filterChain
 			);
 		}
 	}
 
 	protected void doFilterWrapped(
-			ContentCachingRequestWrapper request,
-			ContentCachingResponseWrapper response,
-			FilterChain filterChain
+		ContentCachingRequestWrapper request,
+		ContentCachingResponseWrapper response,
+		FilterChain filterChain
 	) throws ServletException, IOException {
 		try {
 			filterChain.doFilter(request, response);
@@ -60,22 +67,50 @@ public class ApiLoggingFilter extends OncePerRequestFilter {
 	}
 
 	private static void logRequest(ContentCachingRequestWrapper request) throws IOException {
-		String s = getQueryString(request);
-		log.info("Request : {} uri=[{}] content-type[{}]",
-				request.getMethod(), s, request.getContentType());
+		String queryString = getQueryString(request);
+		String contentType = request.getContentType();
+		String checkedContentType = contentTypeCheck(contentType);
 
-		logPayload("Request", request.getContentType(), request.getContentAsByteArray());
+		log.info("Request : {} uri=[{}] content-type[{}]", request.getMethod(), queryString, checkedContentType);
+
+		if (checkedContentType.split(";")[0].equals("multipart/form-data")) {
+			String multipartContent = getMultipartContent(request);
+			logPayload("Request", checkedContentType, multipartContent.getBytes());
+		} else {
+			logPayload("Request", checkedContentType, request.getContentAsByteArray());
+		}
+
+	}
+
+	private static String getMultipartContent(ContentCachingRequestWrapper request) {
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(
+			request.getServletContext());
+		MultipartResolver multipartResolver = context.getBean(MultipartResolver.class);
+		MultipartHttpServletRequest multipartRequest = multipartResolver.resolveMultipart(request);
+		Map<String, MultipartFile> fileMap = multipartRequest.getFileMap();
+
+		StringBuilder mapToString = new StringBuilder("{");
+
+		for (Map.Entry<String, MultipartFile> entry : fileMap.entrySet()) {
+			MultipartFile file = entry.getValue();
+			mapToString.append(
+				String.format(
+					"\"%s\": \"%s (size: %s)\",",
+					entry.getKey(), file.getOriginalFilename(), file.getSize()
+				)
+			);
+		}
+
+		return mapToString.substring(0, mapToString.length() - 1) + "}";
 	}
 
 	private static void logResponse(ContentCachingResponseWrapper response) throws IOException {
+
 		logPayload("Response", response.getContentType(), response.getContentAsByteArray());
 	}
 
 	private static void logPayload(String prefix, String contentType, byte[] rowData) throws IOException {
-		String nullCheck = contentType == null ? "application/json" : contentType;
-		String emptyCheck = nullCheck.isEmpty() ? "application/json" : nullCheck;
-
-		boolean visible = isVisible(MediaType.valueOf(emptyCheck));
+		boolean visible = isVisible(MediaType.valueOf(contentType));
 
 		if (visible) {
 			if (rowData.length > 0) {
@@ -87,6 +122,11 @@ public class ApiLoggingFilter extends OncePerRequestFilter {
 		}
 	}
 
+	private static String contentTypeCheck(String contentType) {
+		String nullCheck = contentType == null ? "application/json" : contentType;
+		return nullCheck.isEmpty() ? "application/json" : nullCheck;
+	}
+
 	private static String getJsonString(byte[] rowData) throws JsonProcessingException {
 		ObjectMapper objectMapper = new ObjectMapper();
 		Object jsonObject = objectMapper.readValue(new String(rowData), Object.class);
@@ -95,15 +135,15 @@ public class ApiLoggingFilter extends OncePerRequestFilter {
 
 	private static boolean isVisible(MediaType mediaType) {
 		final List<MediaType> visibleTypes = Arrays.asList(
-				MediaType.APPLICATION_FORM_URLENCODED,
-				MediaType.APPLICATION_JSON,
-				MediaType.APPLICATION_XML,
-				MediaType.valueOf("application/*+json"),
-				MediaType.valueOf("application/*+xml"),
-				MediaType.MULTIPART_FORM_DATA
+			MediaType.APPLICATION_FORM_URLENCODED,
+			MediaType.APPLICATION_JSON,
+			MediaType.APPLICATION_XML,
+			MediaType.valueOf("application/*+json"),
+			MediaType.valueOf("application/*+xml"),
+			MediaType.MULTIPART_FORM_DATA
 		);
 
 		return visibleTypes.stream()
-				.anyMatch(visibleType -> visibleType.includes(mediaType));
+			.anyMatch(visibleType -> visibleType.includes(mediaType));
 	}
 }
